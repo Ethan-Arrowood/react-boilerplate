8c4605277eb3990adb50f2fe230a19e0c664cd09
Ethan Arrowood <ethan.arrowood@gmail.com>
2016-09-11 21:44:56 -0400

    Refactor app to work with localstorage

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -40,4 +40,21 @@ describe('Actions', () => {
 
     expect(res).toEqual(action);
   });
+
+  it('should generate add todos action', () => {
+    var todos = [{
+      id: '111',
+      text: 'anything',
+      completed: false,
+      completedAt: undefined,
+      createdAt: 33000
+    }];
+    var action = {
+      type: 'ADD_TODOS',
+      todos
+    };
+    var res = actions.addTodos(action.todos);
+
+    expect(res).toEqual(action);
+  })
 });
