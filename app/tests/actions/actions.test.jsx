420625c52c1b128e8ee49b9e22898612d35e9344
Ethan Arrowood <ethan.arrowood@gmail.com>
2016-10-08 14:44:13 -0400

    Add authReducer and actions

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -87,6 +87,25 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
+  it('should generate login action object', () => {
+    const action = {
+      type: 'LOGIN',
+      uid: '123abc'
+    };
+    var res = actions.login(action.uid);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate logout action object', () => {
+    const action = {
+      type: 'LOGOUT'
+    };
+    var res = actions.logout();
+
+    expect(res).toEqual(action);
+  });
+
   describe('Tests with firebase todos', () => {
     var testTodoRef;
 
@@ -146,4 +165,6 @@ describe('Actions', () => {
       done();
     });
   });
+
+
 });
